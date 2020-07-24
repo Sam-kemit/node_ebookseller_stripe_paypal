bee05f6048391a402a853a54c784d3a398fb26fb
Samuel KUETA <samuel.kueta@gmail.com>
2020-07-24 18:22:33 +0200

    add a keys credentials

---
+++ config/userCredential.js
@@ -0,0 +1,7 @@
+const userCredential = {
+    mode: 'sandbox',
+    client_id: 'AWQWlCZjx6ewNvS1DOFJgwkMWYi6lYcBIzCfIJbJl63UI9pH6qHXhVMA-Z7htEnuNaESHBb8tutYkVEe',
+    client_secret: 'ENno5Adf02jH27SyRQE26PoHWFc1aWQ33ck7j2Rhq1Pf9Z6K70VzBfsWJor2aVb2fj9kteBeRLaTSwzq'
+}
+
+module.exports = userCredential;
